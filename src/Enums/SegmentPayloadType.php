7bf217d1d0c537129a03d7575e77df1c56368ecb
Daan Raatjes <daanraatjes@gmail.com>
2022-08-29 11:23:21 +0200

    Update SegmentPayloadType.php

--- src/Enums/SegmentPayloadType.php
+++ src/Enums/SegmentPayloadType.php
@@ -8,9 +8,11 @@ use MyCLabs\Enum\Enum;
  * @psalm-immutable
  * @method static SegmentPayloadType TRACK()
  * @method static SegmentPayloadType IDENTIFY()
+ * @method static SegmentPayloadType GROUP()
  */
 class SegmentPayloadType extends Enum
 {
     private const TRACK = 'track';
     private const IDENTIFY = 'identify';
+    private const GROUP = 'group';
 }
